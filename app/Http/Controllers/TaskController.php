df51e94ecda5545b0edd67c3cc7880c589b19736
Quoc Thanh <pentapperthanh@gmail.com>
2020-04-01 10:34:55 +0700

    create views, layouts, errors, create task, view task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,4 +11,24 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
+    public function index(Request $request)
+    {
+        $tasks = $request->user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
 }
